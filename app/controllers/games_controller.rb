dab2954aa4b7ff5d4d18d50594a6ea0b9b7f9905
georgina puig <georginaxcodes@gmail.com>
2020-06-08 20:03:17 -0500

    logest-word *>

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+# rails generate controller games
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # rails routes 
+  get 'new', to: 'games#new'
+  post 'score', to: 'games#score'
 end
